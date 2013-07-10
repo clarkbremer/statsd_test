5e8654f478a21c85603fbc983805f0c22f396e54
Clark Bremer <clarkbremer@gmail.com>
2013-07-10 12:03:47 -0500

    hard code the metric

--- app/controllers/statsd_controller.rb
+++ app/controllers/statsd_controller.rb
@@ -8,7 +8,7 @@ class StatsdController < ApplicationController
   	metric = params[:q]
   	puts "got statsd increment for #{metric}"
     $statsd.increment 'test.ruby.hardcoded'
-  	flash[:notice] = "statsd event sent"
+  	flash[:notice] = "statsd hardcoded event sent"
   	redirect_to statsd_index_url
 
   end
